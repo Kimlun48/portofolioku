d26ba067d98ee2ddce20039c985d67a1b014ef18
kimlun48 <asepsuherman1984@gmail.com>
2023-10-13 22:34:07 +0700

    first commit7

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function myMenuFunction(){
   
   /* ----- TYPING EFFECT ----- */
   let typingEffect = new Typed(".typedText",{
-    strings : ["Designer","Youtuber","Developer"],
+    strings : ["Programer","Network Enginering","Fullstack Web Developer"],
     loop : true,
     typeSpeed : 100,
     backSpeed : 80,
